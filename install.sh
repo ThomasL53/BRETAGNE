60318c4f33c1fb852a869cb40bb219b0a570ae93
Thomas <thomas.lefeuvre2@thalesgroup.com>
2024-09-05 14:41:08 +0200

    using debian depot for docker + add support of debian 11 and kali

--- install.sh
+++ install.sh
@@ -11,54 +11,58 @@ if ! sudo ls > /dev/null 2>&1; then
 fi
 
 #Distribution check
-if [[ -f /etc/debian_version ]]; then
-    if grep -q "Ubuntu" /etc/os-release; then
-        ENV="Ubuntu"
+#!/bin/bash
+OS=$(lsb_release -si)
+VERSION=$(lsb_release -sr)
+
+if [[ "$OS" == "Debian" ]]; then
+    if [[ "$VERSION" == "11" || "$VERSION" == "12" ]]; then
+        DIST=$VERSION
     else
-        ENV="Debian"
+        echo "This debian version is not supported"
     fi
+elif [[ "$OS" == "Ubuntu" ]]; then
+    DIST="ubuntu"
+elif [[ "$OS" == "Kali" ]]; then
+    DIST="kali"
 else
-    echo "Please use Ubuntu or Debian"
+    echo "Please use debian 11,12, ubuntu or kali"
 fi
 
-if [ "$ENV" = "Ubuntu" ]; then
-  #Install docker
-  echo "Install Docker..." | tee -a "$LOG_FILE"
-  sudo apt-get update -qq >> "$LOG_FILE" 2>&1
-  sudo apt-get install ca-certificates curl -qq >> "$LOG_FILE" 2>&1
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc >> "$LOG_FILE" 2>&1
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-  # Add the repository to Apt sources:
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list >> "$LOG_FILE" 2>&1
-  sudo apt-get update -qq >> "$LOG_FILE" 2>&1
-  sudo apt-get install docker-ce docker-ce-cli containerd.io -yqq >> "$LOG_FILE" 2>&1
-else
-  # Add Docker's official GPG key:
-  echo "Install Docker..." | tee -a "$LOG_FILE"
-  sudo apt-get update -qq >> "$LOG_FILE" 2>&1
-  sudo apt-get install ca-certificates curl -qq >> "$LOG_FILE" 2>&1
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc >> "$LOG_FILE" 2>&1
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
+#Install docker
+echo "Install Docker..." | tee -a "$LOG_FILE"
+sudo apt-get update -qq >> "$LOG_FILE" 2>&1
+sudo apt-get install docker.io -qq >> "$LOG_FILE" 2>&1
 
-  # Add the repository to Apt sources:
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null >> "$LOG_FILE" 2>&1
-  sudo apt-get update -qq >> "$LOG_FILE" 2>&1
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yqq >> "$LOG_FILE" 2>&1
-fi
 
 # Install Kathara
+if [[ "$DIST" == "ubuntu" ]]; then
 echo "Add Kathara depot..." | tee -a "$LOG_FILE"
 sudo apt-get install software-properties-common -yqq >> "$LOG_FILE" 2>&1
 sudo apt update -qq >> "$LOG_FILE" 2>&1
 sudo add-apt-repository ppa:katharaframework/kathara -sy >> "$LOG_FILE" 2>&1
+
+elif [[ "$DIST" == "11" ]]; then
+  wget -qO - "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x21805a48e6cbba6b991abe76646193862b759810" | sudo gpg --dearmor -o /usr/share/keyrings/ppa-kathara-archive-keyring.gpg >> "$LOG_FILE" 2>&1
+  echo "deb [ signed-by=/usr/share/keyrings/ppa-kathara-archive-keyring.gpg ] http://ppa.launchpad.net/katharaframework/kathara/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/kathara.list yqq >> "$LOG_FILE" 2>&1
+  echo "deb-src [ signed-by=/usr/share/keyrings/ppa-kathara-archive-keyring.gpg ] http://ppa.launchpad.net/katharaframework/kathara/ubuntu focal main" | sudo tee -a /etc/apt/sources.list.d/kathara.list yqq >> "$LOG_FILE" 2>&1
+
+elif [[ "$DIST" == "12" ]]; then
+  wget -qO - "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x21805a48e6cbba6b991abe76646193862b759810" | sudo gpg --dearmor -o /usr/share/keyrings/ppa-kathara-archive-keyring.gpg >> "$LOG_FILE" 2>&1
+  echo "deb [ signed-by=/usr/share/keyrings/ppa-kathara-archive-keyring.gpg ] http://ppa.launchpad.net/katharaframework/kathara/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/kathara.list >> "$LOG_FILE" 2>&1
+  echo "deb-src [ signed-by=/usr/share/keyrings/ppa-kathara-archive-keyring.gpg ] http://ppa.launchpad.net/katharaframework/kathara/ubuntu jammy main" | sudo tee -a /etc/apt/sources.list.d/kathara.list >> "$LOG_FILE" 2>&1
+elif [[ "$DIST" == "12" ]]; then
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 21805A48E6CBBA6B991ABE76646193862B759810
+  echo "deb http://ppa.launchpad.net/katharaframework/kathara/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/kathara.list >> "$LOG_FILE" 2>&1
+  echo "deb-src http://ppa.launchpad.net/katharaframework/kathara/ubuntu focal main" | sudo tee -a /etc/apt/sources.list.d/kathara.list >> "$LOG_FILE" 2>&1
+  wget http://ftp.it.debian.org/debian/pool/main/m/mpdecimal/libmpdec2_2.4.2-2_amd64.deb
+  wget http://ftp.de.debian.org/debian/pool/main/libf/libffi/libffi6_3.2.1-9_amd64.deb
+  wget http://ftp.de.debian.org/debian/pool/main/r/readline/libreadline7_7.0-5_amd64.deb
+  sudo apt install ./libmpdec2_2.4.2-2_amd64.deb
+  sudo apt install ./libffi6_3.2.1-9_amd64.deb
+  sudo apt install ./libreadline7_7.0-5_amd64.deb
+fi
+
 sudo apt update -yqq >> "$LOG_FILE" 2>&1
 echo "Install Kathara..." | tee -a "$LOG_FILE"
 sudo apt install kathara -yqq >> "$LOG_FILE" 2>&1
@@ -79,10 +83,8 @@ echo "Cloning the BRETAGNE depot..." | tee -a "$LOG_FILE"
 git clone https://github.com/ThomasL53/BRETAGNE.git -q >> "$LOG_FILE" 2>&1
 
 # Manage Docker as non root user
-sudo usermod -aG docker ${USER} >> "$LOG_FILE" 2>&1
-newgrp docker >> "$LOG_FILE" 2>&1
+sudo usermod -aG docker ${USER} 
 
 # Add env to bashrc
 echo "alias bretagne=\"python3 $(pwd)/BRETAGNE/bretagne.py\"" >> ~/.bashrc
-source ~/.bashrc
-echo "Installation complete. Restart your compture. For more help, use: bretagne -h" | tee -a "$LOG_FILE"
+echo "Installation complete. RESTART YOUR COMPUTRE. For more help, use: bretagne -h" | tee -a "$LOG_FILE"
